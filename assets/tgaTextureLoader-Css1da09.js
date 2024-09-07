ac50b57bf506b5a70abb65f2f89e019b6751ad98
yuan <2368130197@qq.com>
2024-09-07 13:03:24 +0800

    ADD

---
+++ assets/tgaTextureLoader-Css1da09.js
@@ -0,0 +1,1 @@
+import{f as t,g as e}from"./index-n-gux4Qm.js";class a{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(a,s,o){const r=new Uint8Array(a.buffer,a.byteOffset,a.byteLength),n=t(r);o(n.width,n.height,s.generateMipMaps,!1,(()=>{e(s,r)}))}}export{a as _TGATextureLoader};
